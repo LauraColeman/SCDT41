3f9688830610baecac0caa9b3388232c5e01d8f1
LauraColeman <57405611+LauraColeman@users.noreply.github.com>
2020-05-21 15:21:03 +0100

    Create Program.cs

---
+++ CW1/Task2/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quiz test = new Quiz();
+
+
+            test.QuizBegin();
+        }
+    }
+}
